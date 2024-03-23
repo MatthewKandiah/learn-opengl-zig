268c972b7642b5b46b533b654d40db3c2442dce8
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-03-23 17:08:46 +0000

    vertex shader passed to gpu memory and compiled

--- src/main.zig
+++ src/main.zig
@@ -15,11 +15,7 @@ pub fn main() !void {
         c.glfwTerminate();
     }
 
-    _ = c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
-    _ = c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
-    _ = c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
-    // to build on mac, you may need to uncomment this line
-    // _ = c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
+    setWindowHints();
 
     var window = c.glfwCreateWindow(800, 600, "LearnOpenGL", null, null);
     if (window == null) {
@@ -34,10 +30,45 @@ pub fn main() !void {
 
     c.glViewport(0, 0, 800, 600);
 
-    const resizeCallback = c.glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-    _ = resizeCallback;
+    _ = c.glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    const vertices = [9]f32{ -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0 };
+    var vbo: c_uint = undefined;
+    c.glGenBuffers(1, &vbo);
+    c.glBindBuffer(c.GL_ARRAY_BUFFER, vbo);
+    c.glBufferData(c.GL_ARRAY_BUFFER, @sizeOf(@TypeOf(vertices)), &vertices, c.GL_STATIC_DRAW);
+
+    var vertex_shader: c_uint = undefined;
+    vertex_shader = c.glCreateShader(c.GL_VERTEX_SHADER);
+    c.glShaderSource(vertex_shader, 1, &vertex_shader_source, null);
+    c.glCompileShader(vertex_shader);
+    var shader_compilation_succeeded: c_int = undefined;
+    c.glGetShaderiv(vertex_shader, c.GL_COMPILE_STATUS, &shader_compilation_succeeded);
+    if (shader_compilation_succeeded == 0) {
+        var shader_compilation_error_log: [512]u8 = undefined;
+        c.glGetShaderInfoLog(vertex_shader, 512, null, &shader_compilation_error_log);
+        std.debug.panic("Vertex shader compilation failed\n{s}", .{shader_compilation_error_log});
+    }
 
     runRenderLoop(window);
+}
+
+const vertex_shader_source: [*c]const u8 =
+    \\#version 330 core
+    \\layout (location = 0) in vec3 aPos;
+    \\
+    \\void main()
+    \\{
+    \\  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    \\}
+;
+
+fn setWindowHints() void {
+    _ = c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    _ = c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
+    _ = c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+    // to build on mac, you may need to uncomment this line
+    // _ = c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
 }
 
 fn runRenderLoop(window: ?*c.GLFWwindow) void {

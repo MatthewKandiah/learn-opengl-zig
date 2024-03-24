d9ac31905ed035f0fb67e9c88658b1b394060e3d
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-03-24 15:32:31 +0000

    refactor to easily add more executables as we follow the next tutorials

--- build.zig
+++ build.zig
@@ -4,31 +4,46 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "learn-opengl-zig",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(exe);
-
-    // Includes
-    exe.addIncludePath(.{ .path = "deps/include" });
-
-    // Sources
-    const glad_source_file = .{ .file = std.build.LazyPath{ .path = "deps/src/glad.c" }, .flags = &[_][]u8{} };
-    exe.addCSourceFile(glad_source_file);
-
-    // Libraries
-    exe.linkLibC();
-    exe.linkSystemLibrary("glfw3");
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const hello_window = Output{ .name = "hello_triangles", .src = "src/main.zig", .description = "Hello OpenGL Triangles", .target = target, .optimize = optimize };
+    hello_window.build(b);
 }
+
+const Output = struct {
+    name: []const u8,
+    src: []const u8,
+    description: []const u8,
+    target: std.zig.CrossTarget,
+    optimize: std.builtin.Mode,
+
+    const Self = @This();
+
+    fn build(self: Self, b: *std.Build) void {
+        const exe = b.addExecutable(.{
+            .name = self.name,
+            .root_source_file = .{ .path = self.src },
+            .target = self.target,
+            .optimize = self.optimize,
+        });
+        b.installArtifact(exe);
+
+        // Includes
+        exe.addIncludePath(.{ .path = "deps/include" });
+
+        // Sources
+        const glad_source_file = .{ .file = std.build.LazyPath{ .path = "deps/src/glad.c" }, .flags = &[_][]u8{} };
+        exe.addCSourceFile(glad_source_file);
+
+        // Libraries
+        exe.linkLibC();
+        exe.linkSystemLibrary("glfw3");
+
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step(self.name, self.description);
+        run_step.dependOn(&run_cmd.step);
+    }
+};

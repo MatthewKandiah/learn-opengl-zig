b904d9de8242ce06194a60eb8af58faa8ce75eca
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-03-25 17:17:27 +0000

    define vertex colours in our vertex buffer using an extra vertex attribute - get a neat colour gradient due to fragment interpolation!

--- src/shaders.zig
+++ src/shaders.zig
@@ -7,30 +7,34 @@ const c = @cImport({
 });
 
 const single_triangle_vertices = [_]f32{
-    -0.5, -0.5, 0.0,
-    0.5,  -0.5, 0.0,
-    0.0,  0.5,  0.0,
+    // x y z r g b
+    0.5,  -0.5, 0.0, 1.0, 0.0, 0.0,
+    -0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+    0.0,  0.5,  0.0, 0.0, 0.0, 1.0,
 };
 
 const vertex_shader_source: [*c]const u8 =
     \\#version 330 core
     \\layout (location = 0) in vec3 aPos;
+    \\layout (location = 1) in vec3 aColor;
+    \\
+    \\out vec3 ourColor;
     \\
     \\void main()
     \\{
     \\  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    \\  ourColor = aColor;
     \\}
 ;
 
 const fragment_shader_source: [*c]const u8 =
     \\#version 330 core
     \\out vec4 FragColor;
-    \\
-    \\uniform vec4 ourColor;
+    \\in vec3 ourColor;
     \\
     \\void main()
     \\{
-    \\  FragColor = ourColor;
+    \\  FragColor = vec4(ourColor, 1.0);
     \\}
 ;
 
@@ -83,8 +87,12 @@ pub fn main() !void {
     c.glBindVertexArray(vao);
     c.glBindBuffer(c.GL_ARRAY_BUFFER, vbo);
     c.glBufferData(c.GL_ARRAY_BUFFER, @sizeOf(@TypeOf(single_triangle_vertices)), &single_triangle_vertices, c.GL_STATIC_DRAW);
-    c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 3 * @sizeOf(f32), @ptrFromInt(0));
+    // position attribute
+    c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 6 * @sizeOf(f32), @ptrFromInt(0));
     c.glEnableVertexAttribArray(0);
+    // colour attribute
+    c.glVertexAttribPointer(1, 3, c.GL_FLOAT, c.GL_FALSE, 6 * @sizeOf(f32), @ptrFromInt(3 * @sizeOf(f32)));
+    c.glEnableVertexAttribArray(1);
 
     // Draw wireframe
     // c.glPolygonMode(c.GL_FRONT_AND_BACK, c.GL_LINE);
@@ -94,11 +102,7 @@ pub fn main() !void {
         c.glClearColor(0.2, 0.3, 0.3, 1.0);
         c.glClear(c.GL_COLOR_BUFFER_BIT);
 
-        const time_value: f32 = @floatCast(c.glfwGetTime());
-        const green_value = (std.math.sin(time_value) / 2) + 0.5;
-        const vertex_color_location = c.glGetUniformLocation(shader_program, "ourColor");
         c.glUseProgram(shader_program);
-        c.glUniform4f(vertex_color_location, 0.0, green_value, 0.0, 1.0);
 
         // draw single triangle
         c.glBindVertexArray(vao);

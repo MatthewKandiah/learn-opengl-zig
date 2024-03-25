9d742fe046112c71ae85c4a2f53f4d3e16df3a69
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-03-25 19:28:23 +0000

    comptime embed shader source from files

--- src/shaders.zig
+++ src/shaders.zig
@@ -13,30 +13,9 @@ const single_triangle_vertices = [_]f32{
     0.0,  0.5,  0.0, 0.0, 0.0, 1.0,
 };
 
-const vertex_shader_source: [*c]const u8 =
-    \\#version 330 core
-    \\layout (location = 0) in vec3 aPos;
-    \\layout (location = 1) in vec3 aColor;
-    \\
-    \\out vec3 ourColor;
-    \\
-    \\void main()
-    \\{
-    \\  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    \\  ourColor = aColor;
-    \\}
-;
+const vertex_shader_source: [*c]const u8 = @embedFile("simple.vert");
 
-const fragment_shader_source: [*c]const u8 =
-    \\#version 330 core
-    \\out vec4 FragColor;
-    \\in vec3 ourColor;
-    \\
-    \\void main()
-    \\{
-    \\  FragColor = vec4(ourColor, 1.0);
-    \\}
-;
+const fragment_shader_source: [*c]const u8 = @embedFile("simple.frag");
 
 pub fn main() !void {
     const window = try util.init("Hello Triangles");

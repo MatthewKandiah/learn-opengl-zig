e720fa9c014dd38080dbaa5817eafdffb0bf2dc3
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-03-28 21:14:56 +0000

    hopefully that's the image loading dependency and some images set up

--- src/textures.zig
+++ src/textures.zig
@@ -4,6 +4,7 @@ const util = @import("util.zig");
 const c = @cImport({
     @cInclude("glad/glad.h");
     @cInclude("GLFW/glfw3.h");
+    @cInclude("stb_image.h");
 });
 
 const single_triangle_vertices = [_]f32{
